fd60ca2665eea9355fd64b128bb5c88368c237c5
codwerk <louis_widdy@live.fr>
2020-06-30 23:39:52 +0200

    inte html/css add directive v-scroll-reveal

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,129 @@
+<!-- In a component -->
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main>
+ 
+    <section>
+      <div class="container">
+        <h1 v-scroll-reveal.reset="{ delay: 250 }"></h1>
+
+        <div class="row">
+          <transition
+            @before-enter="beforeEnter"
+            @enter="enter"
+          >
+            <p></p>
+          </transition>
+          <transition>
+            <p></p>
+          </transition>
+        </div>
+      </div>
+    </section>
+ 
+    <!-- This section will reveal itself each time it's scrolled into view -->
+    <section v-scroll-reveal.reset>
+      <div class="container">
+        <h1 v-scroll-reveal.reset="{ delay: 100, scale: 2 }"></h1>
+
+        <div class="row">
+
+          <transition>
+            <p></p>
+          </transition>
+          
+          <p></p>
+        </div>
+      </div>
+    </section>
+ 
+    <!-- Element-specific configuration options can be passed like this -->
+    <section v-scroll-reveal.reset="{ delay: 250 }">
+      <div class="container">
+        <h1></h1>
+
+        <div class="row">
+          <p></p>
+          <p></p>
+        </div>
+      </div>
+    </section>
+ 
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  methods: {
+    // ----------
+    // EN ENTRANT
+    // ----------
+
+    beforeEnter(el) {
+      // ...
+      el.style.left = "-100px";
+      el.style.opacity = 0;
+    },
+    // la fonction de rappel done est facultative quand
+    // utilisée en combinaison avec du CSS
+    enter() {
+      // ...
+
+    }
+
   }
 }
-</script>
 
+</script>
+ 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
+@import url('./assets/scss/index.scss');
+
+  section {
+    height: 100vh;
+    color: #fff;
+  }
+
+  h1 {
+    height: 20px;
+    width: 40%;
+    background-color: #fff;
+    border-radius: 20px;
+    margin: 0 auto;
+  }
+
+  p {
+    height: 200px;
+    width: 50%;
+    background-color: #fff;
+    border-radius: 20px;
+  }
+
+  section:nth-child(1) {
+    background-color: #cb3837;
+  }
+
+  section:nth-child(2) {
+    background-color: rgb(120, 33, 117);
+  }
+
+  section:nth-child(3) {
+    background-color: rgb(41, 0, 138);
+  }
+
+  .container {
+    padding: 30px;
+    max-width: 70%;
+    margin: 0 auto;
+  }
+
+  .row {
+    margin-top: 50px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    height: 500px;
+  }
+</style> 
