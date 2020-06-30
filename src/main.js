666c8762cfce6ba302ac6309b23337ff3a17eaaa
codwerk <louis_widdy@live.fr>
2020-06-30 22:37:08 +0200

    import vue-scroll-reveal

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import VueScrollReveal from 'vue-scroll-reveal';
+
 Vue.config.productionTip = false
+
+// Using ScrollReveal's default configuration
+Vue.use(VueScrollReveal);
 
 new Vue({
   render: h => h(App),
